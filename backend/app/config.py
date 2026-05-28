1302fd033b245c4a559b604d6df1face2b9bc6f6
nebulize17 <nebulize17@gmail.com>
2026-05-28 09:58:47 +0700

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,11 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+# Load .env using absolute path of backend directory
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
+
 
 THAID_CLIENT_ID = os.getenv("THAID_CLIENT_ID", "")
 THAID_CLIENT_SECRET = os.getenv("THAID_CLIENT_SECRET", "")
